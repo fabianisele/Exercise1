71fd9d151799da6b0033a677c4c99bf15fa2768c
Fabian Isele <fabian.isele@me.com>
2015-10-21 10:01:13 +0200

    Sheet 1

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /**
  * 
  */
@@ -13,6 +15,12 @@ public class HelloWorld {
 	 */
 	public static void main(String[] args) {
 		System.out.println("Hello ASE2015 - how are you? :)");
+		Scanner in = new Scanner(System.in);
+		System.out.println("Please enter your name:");
+		String name = in.nextLine();
+		in.close();
+		HelloUser user = new HelloUser(name);
+		user.greetUser();
 	}
 
 }
